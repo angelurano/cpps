bffae6d1036774a52eaca521ae96a3de54533c3a
Miguel Angel Garcia Beltran <angelurano00@gmail.com>
2025-12-23 23:40:39 +0100

    Change format style

--- 00/ex00/megaphone.cpp
+++ 00/ex00/megaphone.cpp
@@ -6,21 +6,21 @@
 /*   By: migugar2 <migugar2@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/18 18:48:49 by migugar2          #+#    #+#             */
-/*   Updated: 2025/12/18 23:58:31 by migugar2         ###   ########.fr       */
+/*   Updated: 2025/12/23 20:47:10 by migugar2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
 int main(int argc, char **argv) {
-	if (argc == 1) {
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-	} else {
-		for (int i = 1; i < argc; i++) {
-			for (int j = 0; argv[i][j]; j++) {
-				std::cout << static_cast<char>(std::toupper(argv[i][j]));
-			}
-		}
-		std::cout << std::endl;
-	}
+  if (argc == 1) {
+    std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+  } else {
+    for (int i = 1; i < argc; i++) {
+      for (int j = 0; argv[i][j]; j++) {
+        std::cout << static_cast<char>(std::toupper(argv[i][j]));
+      }
+    }
+    std::cout << std::endl;
+  }
 }
